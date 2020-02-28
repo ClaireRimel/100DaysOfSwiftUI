d0d74629d6b41a45c82e1d278af8ab9a80a14f0f
ClaireRimel <sivadiercl@gmail.com>
2020-02-28 14:22:54 +0100

    New: LengthConversion Created

---
+++ LengthConversion/LengthConversion/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LengthConversion
+//
+//  Created by Claire on 28/02/2020.
+//  Copyright © 2020 Claire Sivadier. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
