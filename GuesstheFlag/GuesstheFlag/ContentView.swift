d140039ae2c06e219a8ef931263d041a0fc43523
ClaireRimel <sivadiercl@gmail.com>
2020-03-04 13:02:31 +0100

    New:  design part with background color using LinearGradient, design the shape for the flag

--- GuesstheFlag/GuesstheFlag/ContentView.swift
+++ GuesstheFlag/GuesstheFlag/ContentView.swift
@@ -9,20 +9,27 @@
 import SwiftUI
 
 struct ContentView: View {
-    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Monaco", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Monaco", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     
-    var correctAnswer = Int.random(in: 0...2)
+    @State private var correctAnswer = Int.random(in: 0...2)
+    
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    @State private var score = 0
     
     var body: some View {
         ZStack {
-            Color.blue.edgesIgnoringSafeArea(.all)
+            LinearGradient(gradient: Gradient(colors: [.red, .purple, .blue, .green]), startPoint:.topLeading, endPoint: .bottomTrailing).edgesIgnoringSafeArea(.all)
             
             VStack(spacing: 30){
                 VStack {
                     Text("Tap the flag of")
                         .foregroundColor(.white)
+                        .frame(minWidth: 0, maxWidth: .infinity)
                     Text(countries[correctAnswer])
                     .foregroundColor(.white)
+                        .font(.largeTitle)
+                        .fontWeight(.black)
                 }
                 
                 ForEach(0..<3) { number in
